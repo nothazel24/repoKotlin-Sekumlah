3a4a2c3f38602625e6352192c33909f233740bf0
ryanntod . <frdnryann@gmail.com>
2024-10-25 02:47:25 +0000

    updates

--- 018_inheritance/app/src/entity/cat.kt
+++ 018_inheritance/app/src/entity/cat.kt
@@ -2,7 +2,7 @@ package app.src.entity
 import app.src.entity.*
 
 // Subclass / class child
-class Cat:Goat() {
+class Cat: Goat(){
     // Class ini akan mewarisi semua metod / atribut dari class yang sebelumnya
     // yaitu di class "goat"
 }
